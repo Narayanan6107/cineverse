bd967987a91b484d2334fcd50b1cec319c1893f3
Narayanan <narayanan6105@gmail.com>
2026-07-22 17:41:14 +0530

    Add login API

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
 import User from "../models/User.js"
 
 const registerUser=async (req,res)=>{
@@ -38,4 +39,46 @@ const registerUser=async (req,res)=>{
     }
 }
 
-export default registerUser
+const loginUser=async (req,res)=>{
+    try{
+        const {email,password}=req.body
+        if (!email || !password){
+            return res.status(400).json({
+                message:"Email and password are requried"
+            })
+        }
+        const user=await User.findOne({email})
+        if (!user){
+            return res.status(400).json({
+                message:"invalid credentials"
+            })
+        }
+        const isMatch=await bcrypt.compare(password,user.password);
+        if (!isMatch){
+            return res.status(400).json({
+                message:"invalid credentials"
+            })
+        }
+        const token=jwt.sign(
+            {id:user._id},
+            process.env.JWT_SECRET,
+            {expiresIn:"7d"}
+        )
+
+        res.status(200).json({
+            message:"login successful",
+            token,
+            user:{
+                id:user._id,
+                name:user.name,
+                email:user.email
+            }
+        })
+    }catch(error){
+        console.error(error)
+        res.status(500).json({
+            message:"Server error"
+        })
+    }
+}
+export {registerUser,loginUser}
